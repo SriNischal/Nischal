e4daa981e83f916295c50a199f93a58f1428d401
SriNischal <53328356+SriNischal@users.noreply.github.com>
2019-07-30 14:59:30 +0530

    Update Initial.java

--- Calculator/src/Nischal/Initial.java
+++ Calculator/src/Nischal/Initial.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 class Initial {
 	protected ArrayList<String[]> values() {
 		ArrayList<String[]> Employee_Db = new ArrayList<String[]>(100);
-		String Employee1[] = { "1124", "Anusha", "22", "trainee", "java", "20000" };
-		String Employee2[] = { "1128", "Siva", "23", "trainee", "java", "25000" };
+		String Employee1[] = { "1044", "Nischal", "21", "trainee", "java", "20000" };
+		String Employee2[] = { "1163", "Suraj", "21", "trainee", "java", "25000" };
 		Employee_Db.add(Employee1);
 		Employee_Db.add(Employee1);
 		return Employee_Db;
